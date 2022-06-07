55099fe4460a1475e649a059a785a4b11743010c
Jost Alemann <jost_alemann@me.com>
2022-06-07 10:47:06 +0200

    add TODOs feat: modify to use our environment

--- pz-algorithms/Double-DQN/main.py
+++ pz-algorithms/Double-DQN/main.py
@@ -2,7 +2,7 @@ import torch
 import torch.optim as optim
 import torch.nn.functional as F
 from itertools import count
-from env import EnvManager
+from environment import qchess_env
 from epsilon_greedy_strategy import EpsilonGreedyStrategy
 from agent import Agent
 from experience_replay import ReplayMemory, Experience, extract_tensors
@@ -23,15 +23,15 @@ num_episodes = 1000  # Num of episode
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-em = EnvManager(device, 'CartPole-v0')
+env = qchess_env.env()
 strategy = EpsilonGreedyStrategy(eps_start, eps_end, eps_decay)
-agent = Agent(strategy, em.num_actions_available(), device)
+agent = Agent(strategy, env.action_space_size, device)
 memory = ReplayMemory(memory_size)
 
-policy_net = Network(em.num_state_features(),
-                     em.num_actions_available()).to(device)
-target_net = Network(em.num_state_features(),
-                     em.num_actions_available()).to(device)
+policy_net = Network(env.state_space_size,
+                     env.action_space_size).to(device)
+target_net = Network(env.state_space_size,
+                     env.action_space_size).to(device)
 target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 optimizer = optim.Adam(params=policy_net.parameters(), lr=lr)
@@ -40,12 +40,13 @@ optimizer = optim.Adam(params=policy_net.parameters(), lr=lr)
 episode_durations = []
 
 for episode in range(num_episodes):
-    em.reset()
-    state = em.get_state()
+    env.reset()
+    state = env.observe()
     for timestep in count():
         action = agent.select_action(state, policy_net)
-        reward = em.take_action(action)
-        next_state = em.get_state()
+        # TODO: change to receive returns of step (not just reward)
+        reward = env.step(action)
+        next_state = env.observe()
         memory.push(Experience(state, action, next_state, reward))
         state = next_state
         if memory.can_provide_sample(batch_size):
@@ -62,6 +63,7 @@ for episode in range(num_episodes):
             loss.backward()
             optimizer.step()
 
+        # TODO: change to something like env.done (idk if we have that)
         if em.done:
             episode_durations.append(timestep)
             plot(episode_durations, 100)
@@ -72,4 +74,4 @@ for episode in range(num_episodes):
 
     if get_moving_average(100, episode_durations)[-1] >= 195:
         break
-em.close()
+env.close()
